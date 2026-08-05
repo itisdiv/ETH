d2f7050d68833c943246d51da7f3cb62cbd9f8b2
Divya <divyaverma.0219@gmail.com>
2026-08-05 22:28:45 +0530

    First commit

---
+++ models/marts/eth_activity_per_day.sql
@@ -0,0 +1,13 @@
+select
+date,
+transaction_category,
+count(*) as tx_count,
+{{ ethereum_conversion('value') }} as sum_eth_value
+
+from {{ ref('int_transactions_enriched') }}
+
+group by 
+date,
+transaction_category
+
+
